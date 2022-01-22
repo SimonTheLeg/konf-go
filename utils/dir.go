304738fd84e82c576fb9a3fb8030f3a909e93756
Simon Bein <simontheleg@gmail.com>
2022-01-22 22:37:09 +0100

    fix: set correct filepermissions on folders

--- utils/dir.go
+++ utils/dir.go
@@ -8,12 +8,12 @@ import (
 // EnsureDir makes sure that konf store and active dirs exist
 func EnsureDir(f afero.Fs) error {
 
-	err := f.MkdirAll(viper.GetString("storeDir")+"/", 0770)
+	err := f.MkdirAll(viper.GetString("storeDir")+"/", 0600)
 	if err != nil {
 		return err
 	}
 
-	err = f.MkdirAll(viper.GetString("activeDir")+"/", 0770)
+	err = f.MkdirAll(viper.GetString("activeDir")+"/", 0600)
 	if err != nil {
 		return err
 	}
