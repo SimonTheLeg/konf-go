cd02675befe6db8ccead7f837711a65d8ee5cd70
Simon Bein <simontheleg@gmail.com>
2022-01-27 21:11:16 +0100

    fix: konfDir permissions so we can create folders inside

--- utils/dir.go
+++ utils/dir.go
@@ -8,16 +8,17 @@ import (
 )
 
 const KonfPerm fs.FileMode = 0600 // based on the standard file-permissions for .kube/config
+const KonfDirPerm fs.FileMode = 0700 // needed so we can create folders inside
 
 // EnsureDir makes sure that konf store and active dirs exist
 func EnsureDir(f afero.Fs) error {
 
-	err := f.MkdirAll(viper.GetString("storeDir")+"/", KonfPerm)
+	err := f.MkdirAll(viper.GetString("storeDir")+"/", KonfDirPerm)
 	if err != nil {
 		return err
 	}
 
-	err = f.MkdirAll(viper.GetString("activeDir")+"/", KonfPerm)
+	err = f.MkdirAll(viper.GetString("activeDir")+"/", KonfDirPerm)
 	if err != nil {
 		return err
 	}
